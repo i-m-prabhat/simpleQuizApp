a40ef62660f56180c9b5684a5b28106af0079ff7
Prabhat Patel <prabhatpatel508@outlook.com>
2023-01-15 17:53:48 +0530

    structure of thw quiz app created and styled

---
+++ script.js
@@ -0,0 +1,22 @@
+const startButton = $("start-btn");
+const nextButton = $("next-btn");
+
+const questionContainerElement = $("question-container");
+const questionElement = $("question");
+const answerButtonElement = $("answer-button");
+
+let suffle
+
+
+
+
+
+
+
+
+
+
+function $(element)
+{
+    return document.getElementById(element);
+}
